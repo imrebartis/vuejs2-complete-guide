79525b1ddd2ccbf6295def71ce64981e9b6a2f15
Imre Bartis <imre.bartis@siili.com>
2018-04-03 05:50:36 +0300

    creating & using mixins

--- filters/src/List.vue
+++ filters/src/List.vue
@@ -3,38 +3,21 @@
         <div class="row">
             <div>
                 <h1>Filters & Mixins</h1>
-                <p>{{ text | toUppercase | to-lowercase }}</p>
                 <hr>
                 <input v-model="filterText">
                 <ul>
                     <li v-for="fruit in filteredFruits">{{ fruit }}</li>
                 </ul>
-                <hr>
-                <app-list></app-list>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import List from './List.vue';
     import { fruitMixin } from './fruitMixin';
 
     export default {
-        mixins: [fruitMixin],
-        data() {
-            return {
-                text: 'Hello there!'
-            }
-        },
-        filters: {
-            toUppercase(value) {
-                return value.toUpperCase();
-            }
-        },
-        components: {
-            appList: List
-        }
+        mixins: [ fruitMixin ]
     }
 </script>
 

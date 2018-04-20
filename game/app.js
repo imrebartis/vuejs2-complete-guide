51dafe847c91ecca29c783ae972c1b6ab9af5bac
Imre Bartis <imre.bartis@siili.com>
2018-04-20 05:50:57 +0300

    refactoring

--- game/app.js
+++ game/app.js
@@ -12,26 +12,16 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function() {
-            var max = 10;
-            var min = 3;
-            // getting a random number btw 3 & 10:
-            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.monsterHealth -= damage;
+            this.monsterHealth -= this.calculateDamage(3, 10);
 
-            if(this.monsterHealth <= 0) {
-                alert('you win');
-                this.gameIsRunning = false;
+            if (this.checkWin()) {
+                //game's over
                 return;
             }
 
-            max = 12;
-            min = 5;
-            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.playerHealth -= damage;
-            if(this.playerHealth <= 0) {
-                alert('you lose');
-                this.gameIsRunning = false;
-            }
+            this.playerHealth -= this.calculateDamage(5, 12);
+            //check if you won:
+            this.checkWin();
 
         },
         specialAttack: function() {
@@ -42,6 +32,28 @@ new Vue({
         },
         giveUp: function() {
 
+        },
+        calculateDamage: function(min, max) {
+             // getting a random number btw 3 & 10:
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                if(confirm('You win! New game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+            return true;
+          } else if (this.playerHealth <= 0 ) {
+                if(confirm('You lose! New game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+            return true;   
+          }
+          return false;
         }
     }
 

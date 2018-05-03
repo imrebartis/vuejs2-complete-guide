1061a67c11406f106603793f92509f4448bd8e99
Imre Bartis <imre.bartis@siili.com>
2018-05-03 06:42:32 +0300

    vuejs instance lifecycle

--- app.js
+++ app.js
@@ -1,63 +1,37 @@
-var data = {
-  title: 'the VueJS Instance',
-  showParagraph: false
-}
 
-Vue.component('hello', {
-  template: '<h1>Hello!!!</h1>'
-});
 
-var vm1 = new Vue({
-    data: data,
+new Vue({
+    el: '#app',
+    data: {
+      title: 'the VueJS instance'
+    },
+    beforeCreate: function() {
+      console.log('beforeCreate()')
+    },
+    created: function() {
+      console.log('created()')
+    },
+    beforeMount: function() {
+      console.log('beforeMount()')
+    },
+    mounted: function() {
+      console.log('mounted()')
+    },
+    beforeUpdate: function() {
+      console.log('beforeUpdate()')
+    },
+    updated: function() {
+      console.log('updated()')
+    },
+    beforeDestroy: function() {
+      console.log('beforeDestroy()')
+    },
+    destroyed: function() {
+      console.log('destroyed()')
+    },
     methods: {
-      show: function() {
-        this.showParagraph = true;
-        this.updateTitle('The VueJS Instance (Updated)');
-        this.$refs.myButton.innerText = 'Test';
-      },
-      updateTitle: function(title) {
-        this.title = title;
-      }
-    },
-    computed: {
-      lowercaseTitle: function() {
-        return this.title.toLowerCase();
-      }
-    },
-    watch: {
-      title: function(value) {
-        alert('Title changed, new value: ' + value);
+      destroy: function() {
+        this.$destroy()
       }
     }
   });
-
-  vm1.$mount('#app1');
-
-  console.log(vm1.$data === data)
-  // this will be overwrittenby setTimeout's vm1.show():
-  vm1.$refs.myButton.innerText = "new stuff"
-
-  setTimeout(function() {
-    vm1.title = "Changed by Timer";
-    vm1.show()
-  }, 3000);
-
-  var vm2 = new Vue({
-    el: '#app2',
-    data: {
-        title: 'the second instance'
-    },
-    methods: {
-      onChange: function() {
-        vm1.title = "Changed";
-      }
-    }
-  })
-
-  var vm3 = new Vue({
-    template: '<h1>Hello</h1>'
-  })
-
-  // vm3.$mount('#app3')
-  vm3.$mount()
-  document.getElementById('app3').appendChild(vm3.$el)

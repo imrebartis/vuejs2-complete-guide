1333b1b79fbbf9f321e687e6a93c0a1f7073848d
Imre Bartis <imre.bartis@siili.com>
2018-05-06 05:43:31 +0300

    intro to components

--- app.js
+++ app.js
@@ -1,37 +1,14 @@
 
 
-new Vue({
-    el: '#app',
-    data: {
-      title: 'the VueJS instance'
-    },
-    beforeCreate: function() {
-      console.log('beforeCreate()')
-    },
-    created: function() {
-      console.log('created()')
-    },
-    beforeMount: function() {
-      console.log('beforeMount()')
-    },
-    mounted: function() {
-      console.log('mounted()')
-    },
-    beforeUpdate: function() {
-      console.log('beforeUpdate()')
-    },
-    updated: function() {
-      console.log('updated()')
-    },
-    beforeDestroy: function() {
-      console.log('beforeDestroy()')
-    },
-    destroyed: function() {
-      console.log('destroyed()')
-    },
-    methods: {
-      destroy: function() {
-        this.$destroy()
-      }
+Vue.component('my-cmp', {
+  data: function() {
+    return {
+      status: 'Critical'
     }
+  },
+  template: '<p>Server status: {{status}}</p>'
+})
+
+new Vue({
+    el: '#app'
   });

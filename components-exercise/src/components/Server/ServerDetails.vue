8148168afda5446d998387b89373fc0fb59a5048
Imre Bartis <imre.bartis@siili.com>
2018-05-23 07:19:25 +0300

    component communication exercise

--- components-exercise/src/components/Server/ServerDetails.vue
+++ components-exercise/src/components/Server/ServerDetails.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
+                <p v-if="!server">Please select a server</p>
+                <p v-else>Server # {{server.id}} selected, Status: {{server.status}}</p>
+                <hr>
+                <button @click="resetStatus">Change to normal</button>
     </div>
 </template>
 <style scoped>
@@ -8,3 +11,27 @@
         border: 1px solid red;
     }
 </style>
+<script>
+    import {serverBus} from '../../main'
+
+    export default {
+        data: function() {
+            return {
+                server: null
+            }
+        },
+        methods: {
+            // object is a reference type, that's why we don't need to pass this back to servers
+            // (each server is an object, see Servers.vue data function)
+            resetStatus(){
+                this.server.status = 'Normal'
+            }
+        },
+        created() {
+            serverBus.$on('serverSelected', (server) => {
+                this.server = server
+            });
+
+        }
+    }
+</script>

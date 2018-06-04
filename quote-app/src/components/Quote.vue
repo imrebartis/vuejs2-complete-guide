b5a521c700b34c17cfa6c66a590724c4d296d781
Imre Bartis <imre.bartis@siili.com>
2018-06-04 07:13:59 +0300

    new quote component

--- quote-app/src/components/Quote.vue
+++ quote-app/src/components/Quote.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="col-sm-6 col-md-4 col-lg-3">
-        <div class="card" style="width: 18rem;">
+        <div class="panel panel-default">
             <div class="card-body quote">
                 <slot></slot>
             </div>

a56a418f315bed3a207e4b10fb605519a9377885
Imre Bartis <imre.bartis@siili.com>
2018-06-22 19:37:18 +0300

    intro to directives

---
+++ directives/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>built-in directives</h1>
+                        <p v-text="'some text'"></p>
+                        <p v-html="'<strong>some heavy text</strong'"></p>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>custom directives</h1>
+                        <p>color this</p>
+                </div>
+            </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+
+</style>
---
+++ directives/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight')
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})

9e58ffbdcec465dc554a565e97ea2dac1adfd93e
Imre Bartis <imre.bartis@siili.com>
2018-06-23 18:00:57 +0300

    create a simple directive

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -11,7 +11,7 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <h1>custom directives</h1>
-                        <p>color this</p>
+                        <p v-highlight>color this</p>
                 </div>
             </div>
     </div>

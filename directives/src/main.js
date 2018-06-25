3941fc2b590ca3ce75ecde59dc9ceb4d4f8911d3
Imre Bartis <imre.bartis@siili.com>
2018-06-25 12:42:00 +0300

    pass arguments to custom directives

--- directives/src/main.js
+++ directives/src/main.js
@@ -4,7 +4,14 @@ import App from './App.vue'
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
     // el.style.backgroundColor = 'green'
-    el.style.backgroundColor = binding.value
+    // el.style.backgroundColor = binding.value
+    // if v-highlight:background:
+    if (binding.arg === "background") {
+      el.style.backgroundColor = binding.value
+    } else {
+      // if v-highlight:
+      el.style.color = binding.value
+    }
   }
 })
 

d977c062bceec920b67b88784c91bd45e091f489
Imre Bartis <imre.bartis@siili.com>
2018-07-16 07:16:52 +0300

    set up super quiz app project

---
+++ super-quiz/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Question from './components/Question.vue';
+    import Answer from './components/Answer.vue';
+
+    export default {
+        data() {
+            return {
+            }
+        }
+    }
+</script>
+

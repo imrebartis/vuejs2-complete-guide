ebcbea7e2c6f0deb8bdb46d2000584b3610b3308
Imre Bartis <imre.bartis@siili.com>
2018-07-30 07:55:33 +0300

    set up & load routes

---
+++ router/src/routes.js
@@ -0,0 +1,8 @@
+import User from './components/user/User.vue'
+import Home from './components/user/Home.vue'
+
+export const routes = [
+    {path: '/', component: Home },
+    {path: '/user', component: User },
+    
+]

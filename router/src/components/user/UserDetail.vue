b90628765b9561a7ac0bd86e51a6f8022b99aa1b
Imre Bartis <imre.bartis@siili.com>
2018-08-11 07:01:30 +0300

    use query params

--- router/src/components/user/UserDetail.vue
+++ router/src/components/user/UserDetail.vue
@@ -4,7 +4,7 @@
          <p>User loaded has id: {{$route.params.id}}</p>
          <router-link 
             tag="button" 
-            :to="{name: 'userEdit', params: { id: $route.params.id }}"
+            :to="{name: 'userEdit', params: { id: $route.params.id }, query: { locale : 'en', q : 100 }}"
             class="btn btn-primary">Edit user</router-link>
     </div>
 </template>

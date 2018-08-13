7c8652673d50ec96542f9847ac4390ee348475f8
Imre Bartis <imre.bartis@siili.com>
2018-08-13 06:57:51 +0300

    use redirecting

--- router/src/routes.js
+++ router/src/routes.js
@@ -24,6 +24,7 @@ export const routes = [
         { path: ':id', component: UserDetail},
         { path: ':id/edit', component: UserEdit, name: 'userEdit'}
     ]},  
+    { path: '/redirect-me', redirect: { name: 'home' }}
     // {path: '/user/:id', component: User, props: dynamicPropsFn},   
 ]
 

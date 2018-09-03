1e903d721c12c004d348e814ed96b12958b648f8
Imre Bartis <imre.bartis@siili.com>
2018-09-03 06:54:59 +0300

    create the stocks component

--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Stocks component</h1>
+    <div>
+        <app-stock v-for="stock in stocks"></app-stock>
+    </div>     
 </template>
+
+<script>
+    import Stock from './Stock.vue';
+
+    export default {
+        data() {
+            return {
+                stocks: [
+                    {id: 1, name: 'Tesla', price: 110},
+                    {id: 1, name: 'Google', price: 230},
+                    {id: 1, name: 'Apple', price: 250},
+                    {id: 1, name: 'Twitter', price: 9}
+                ]
+            }
+        },
+        components: {
+            appStock: Stock
+        }
+    }
+</script>
+

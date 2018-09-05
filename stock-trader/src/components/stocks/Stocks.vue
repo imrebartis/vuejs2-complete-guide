bc7369c822ce9d81a05de64350304a1027ee159c
Imre Bartis <imre.bartis@siili.com>
2018-09-05 07:07:36 +0300

    set up the vuex state management

--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -8,18 +8,13 @@
     import Stock from './Stock.vue';
 
     export default {
-        data() {
-            return {
-                stocks: [
-                    {id: 1, name: 'Tesla', price: 110},
-                    {id: 1, name: 'Google', price: 230},
-                    {id: 1, name: 'Apple', price: 250},
-                    {id: 1, name: 'Twitter', price: 9}
-                ]
-            }
-        },
         components: {
             appStock: Stock
+        },
+        computed: {
+            stocks() {
+                return this.$store.getters.stocks;
+            }
         }
     }
 </script>
---
+++ stock-trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'Tesla', price: 110},
+    {id: 1, name: 'Google', price: 230},
+    {id: 1, name: 'Apple', price: 250},
+    {id: 1, name: 'Twitter', price: 9}
+]

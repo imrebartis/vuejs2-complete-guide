76daec8053e0e6312465e363bffde3cef6f96986
Imre Bartis <imre.bartis@siili.com>
2018-09-15 06:47:21 +0300

    use filters with stocks

--- stock-trader/src/main.js
+++ stock-trader/src/main.js
@@ -5,7 +5,11 @@ import App from './App.vue'
 import { routes } from './routes'
 import store from './store/store';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString('fi');
+});
 
 const router = new VueRouter({
   mode: 'history',
